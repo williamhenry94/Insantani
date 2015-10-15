c2d8a0f5998001d0cce95ef1a2c70f4944ca8cf1
williamhenry94 <william.hidayat@gmail.com>
2015-10-15 23:26:11 +0700

    Callback value
    to validate the JSON output of the Backend dedicated only for article
    and edit quanitity in shopping cart

---
+++ app.js
@@ -0,0 +1,44 @@
+var should =require('chai').should(),
+    expect=require('chai').expect,
+    supertest=require('supertest'),
+    api=supertest('http://localhost:3000')
+//var assert = require('assert');
+describe('Article', function() {
+    it('should be an object with keys and values', function (){
+        api.get('/feed/1')
+        .set('Accept','application/json')
+        .expect(200)
+        .end(function(err,res){
+            expect(res.body).to.have.property("article_id");
+expect(res.body.article_id).to.not.equal(null);
+expect(res.body).to.have.property("author");
+expect(res.body.author).to.not.equal(null);
+expect(res.body).to.have.property("article_title"); expect(res.body.article_title).to.not.equal(null);
+expect(res.body).to.have.property("picture");
+expect(res.body).to.have.property("content");
+expect(res.body.content).to.not.equal(null);
+done();
+        });
+     
+    });
+});
+    
+describe ('Shopping Cart',function(){
+    it('should be updated with a new quantity', function (){
+        api.put('/shopping_cart/cart/1WQ46710DS0/edit/1')
+        .set('Accept','application/json')
+        .send({
+            product_id: 1,
+            farmer:"Izhar Almizan",
+            quantity:3,    
+        })
+        .expect(201)
+        .end(function(err,res){
+            expect(res.body.product_id).to.equal(1);
+expect(res.body.farmer).to.equal("Izhar Almizan");
+expect(res.body.quantity).to.equal(3);
+done();
+        });
+     
+    });
+  });
