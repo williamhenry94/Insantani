0af67627006e1ad78100481c728444fa1b35fc5d
williamhenry94 <william.hidayat@gmail.com>
2015-10-19 16:24:59 +0700

    move,edit
    move edit

--- Insantani_Project/test/app.js
+++ Insantani_Project/test/app.js
@@ -13,7 +13,8 @@ describe('Article', function() {
 expect(res.body.article_id).to.not.equal(null);
 expect(res.body).to.have.property("author");
 expect(res.body.author).to.not.equal(null);
-expect(res.body).to.have.property("article_title"); expect(res.body.article_title).to.not.equal(null);
+expect(res.body).to.have.property("article_title"); 
+expect(res.body.article_title).to.not.equal(null);
 expect(res.body).to.have.property("picture");
 expect(res.body).to.have.property("content");
 expect(res.body.content).to.not.equal(null);
@@ -24,18 +25,30 @@ done();
 });
     
 describe ('Shopping Cart',function(){
+    var product_id;
+    var farmer;
+    var quantity;
     it('should be updated with a new quantity', function (){
+        api.get('/shopping_cart/cart/1WQ46710DS0')
+        .set('Accept','application/json')
+        .expect(200)
+        .end(function(err,res){
+            product_id=res.body.product_id;
+            farmer=res.body.farmer;
+            quantity=res.body.quantity;
+            });
+        
         api.put('/shopping_cart/cart/1WQ46710DS0/edit/1')
         .set('Accept','application/json')
         .send({
             product_id: 1,
             farmer:"Izhar Almizan",
-            quantity:3,    
+            quantity:3    
         })
         .expect(201)
         .end(function(err,res){
-            expect(res.body.product_id).to.equal(1);
-expect(res.body.farmer).to.equal("Izhar Almizan");
+expect(res.body.product_id).to.equal(product_id);
+expect(res.body.farmer).to.equal(farmer);
 expect(res.body.quantity).to.equal(3);
 done();
         });
